22a0e99d64cdb157b8825dcc3591b884c4ca57b9
Kim Thompson <kimthompson@hey.com>
2022-09-30 16:21:07 -0500

    Initial Commit

---
+++ AlertWrapperDemo/AlertWrapperDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlertWrapperDemoApp.swift
+//  AlertWrapperDemo
+//
+//  Created by Kimberly Thompson on 9/30/22.
+//
+
+import SwiftUI
+
+@main
+struct AlertWrapperDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
